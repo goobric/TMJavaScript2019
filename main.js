42fcdfa61ddf187a5c39f57687d3595e9b1a0b35
goobric aka Mikal <goobric@gmail.com>
2022-05-30 22:17:32 +0200

    defining variables with var, let, const

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("Hello Coders");
+console.error('This is an error');
+console.warn('This is a warning');
+
+// var, let, const
+// var is a globally scoped variable
+// let & const were added with ES6
+
+// let, is used for re-assigning values
+let age = 30;
+age = 31;
+
+console.log(age);
+
+let score;
+score = 10;
+console.log(score);
+
+// const, is abbreviation for constant, it can not be re-assigned and changed
+
+const name = "Brad Travsery"
